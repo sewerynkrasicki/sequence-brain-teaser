e50b0cbd461eebef58907746e414afb0daac52b4
Seweryn <seweryn.krasicki@protonmail.com>
2019-12-30 22:03:29 +0100

    fileChooser added

--- src/board/menuBar.java
+++ src/board/menuBar.java
@@ -1,6 +1,7 @@
 package board;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,6 +19,8 @@ public class menuBar extends JMenuBar {
     JMenuItem m4i = new JMenuItem("Load");
     JMenu m3 = new JMenu("Undo");
     JMenu m4 = new JMenu("Redo");
+    JFileChooser jfcSave = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+    JFileChooser jfcLoad = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
 
     menuBar() {
         add(m1);
@@ -28,8 +31,39 @@ public class menuBar extends JMenuBar {
         m1.add(m2i);
         m1.add(m3i);
         m1.add(m4i);
+        m3i.addActionListener(new saveFile());
+        m4i.addActionListener(new loadFile());
         setPreferredSize(new Dimension(50, 50));
         m1i.addActionListener(new popUp());
+    }
+
+    class saveFile implements ActionListener
+    {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            jfcSave.setDialogTitle("Choose a directory to save your file: ");
+            jfcSave.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int returnValue = jfcSave.showSaveDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                if (jfcSave.getSelectedFile().isDirectory()) {
+                    System.out.println("You selected the directory: " + jfcSave.getSelectedFile());
+                }
+            }
+        }
+    }
+
+    class loadFile implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            int returnValue = jfcLoad.showOpenDialog(null);
+            if(returnValue == JFileChooser.APPROVE_OPTION)
+            {
+                File selectedFile = jfcLoad.getSelectedFile();
+                System.out.println(selectedFile.getAbsolutePath());
+            }
+        }
     }
 }
 
